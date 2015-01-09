596f41587d321487c00dad1aa372a95dd4cb8bb5
Ben Lippincott <benji.lippincott@gmail.com>
2015-01-08 21:04:10 -0500

    crap

--- crapinstall.sh
+++ crapinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[hash curl 2>/dev/null]]; then
-	wget -O /usr/bin/local/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
+	wget -O /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
 	wget -O ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 else
-    curl -o /usr/bin/local/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
+    curl -o /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
 	curl -o ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 fi
 exit
