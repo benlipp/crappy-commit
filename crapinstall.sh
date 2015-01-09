ec0b1d6f9ddcac12fcb454760df85b3b6d3f92d5
jonnolen <jon.nolen+github@gmail.com>
2015-01-09 11:24:01 -0500

    Update crapinstall.sh

--- crapinstall.sh
+++ crapinstall.sh
@@ -7,6 +7,6 @@ else
     curl -o /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
 	curl -o ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 fi
-chmod 0755 /usr/local/bin/crap
+chmod 0755 /usr/local/bin/hemorrhoid
 chmod 0664 ~/.crap_messages.txt
 exit
