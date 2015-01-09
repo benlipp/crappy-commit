2d435e078162722d880cc96acf51cc771efdca98
Ben Lippincott <benji.lippincott@gmail.com>
2015-01-08 21:16:53 -0500

    forgot we're not using a smart language

--- crapinstall.sh
+++ crapinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-if [[hash curl 2>/dev/null]]; then
+hash curl &> /dev/null
+if [ $? -eq 1 ]; then
 	wget -O /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
 	wget -O ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 else
@@ -8,4 +8,5 @@ else
 	curl -o ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 fi
 chmod 0755 /usr/local/bin/crap
+chmod 0664 ~/.crap_messages.txt
 exit
