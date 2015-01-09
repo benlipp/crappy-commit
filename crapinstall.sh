25855574673ceb968792014cd241cf5f1b04120e
jonnolen <jon.nolen+github@gmail.com>
2015-01-09 11:35:47 -0500

    Update crapinstall.sh

--- crapinstall.sh
+++ crapinstall.sh
@@ -2,11 +2,14 @@
 hash curl &> /dev/null
 if [ $? -eq 1 ]; then
 	wget -O /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
+	wget -O /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/hemorrhoid
 	wget -O ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 else
     curl -o /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/crap
-	curl -o ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
+    curl -o /usr/local/bin/crap https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/hemorrhoid
+    curl -o ~/.crap_messages.txt https://raw.githubusercontent.com/obiwanakin/crappy-commit/master/messages.txt
 fi
+chmod 0755 /usr/local/bin/crap
 chmod 0755 /usr/local/bin/hemorrhoid
 chmod 0664 ~/.crap_messages.txt
 exit
